545d8128d1541324d6776b47782d1265fa9e450f
jsteiner207 <jsteiner@stcharlessd.org>
2014-09-26 03:54:50 -0500

    fgbdfhdhdh

--- Guess_My_Number.py
+++ Guess_My_Number.py
@@ -1,11 +1,11 @@
 # Program Name:
-# Author:
+# Author:sgsfdsdgf
 # Date:
 
 # Guess My Number
 #
 # The computer picks a random number between 1 and 100
-# The player tries to guess it and the computer lets
+# The player tries to guess itgit and the computer lets
 # the player know if the guess is too high, too low
 # or right on the money
 
