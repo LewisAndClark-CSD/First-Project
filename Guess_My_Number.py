95ce8455e035f240d354d7df1925dd8a480043e6
DennisGordic <dgordick@stcharlessd.org>
2014-09-26 03:54:26 -0500

    modified:   Guess_My_Number.py

--- Guess_My_Number.py
+++ Guess_My_Number.py
@@ -1,6 +1,6 @@
-# Program Name:
-# Author:
-# Date:
+# Program Name: Guess_My_Number
+# Author: Dennis Gordick
+# Date: 9/26/2014
 
 # Guess My Number
 #
@@ -12,25 +12,31 @@
 import random  
 
 #Opening Remarks
-print("\tWelcome to 'Guess My Number'!")
-print("\nI'm thinking of a number between 1 and 100.")
-print("Try to guess it in as few attempts as possible.\n")
+print("Welcome to 'Guess My Number'!")
+print("I'm thinking of a number between 1 and 100.")
+print("Try to guess it in as few attempts as possible.")
 
 # set the initial values
 the_number = random.randint(1, 100)
-guess = # Create the priming read here
+# Create the priming read here
+guess = input("Enter your guess:")
 tries = 1
 
+if int(guess) == int(the_number):
+    print("Your right on the money!")
+elif int(guess) > int(the_number):
+    print("To high!")
+elif int(guess) < int(the_number):
+    print("To low!")
 # guessing loop
 while guess != the_number:
-    #put the guessing game logic here
-            
     tries += 1
-    guess = #Create the following read here
-
-
+    #put the guessing game logic here
+    #Create the following read here
+    guess = input("Guess again!")
+#Didnt know how to make it reloop to the start...
 print("You guessed it!  The number was", the_number)
-print("And it only took you", tries, "tries!\n")
+print("And it only took you", tries, "tries!")
 
 #Program Closing  
-input("\n\nPress the enter key to exit.")
+input("Press the enter key to exit.")
